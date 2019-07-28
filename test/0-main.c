880f7bbabf7132d8a73b1a3b12eff1ba0e6e5af8
mrdoomus <mrdoom.official@gmail.com>
2019-07-28 18:10:34 +0000

    Features: Added new case test %z

--- test/0-main.c
+++ test/0-main.c
@@ -10,6 +10,8 @@
 int main(void)
 {
 	_printf("Let's try to printf a %% simple sentence.\n");
+	_printf("This is a test %z.\n");
 	printf("Let's try to printf a %% simple sentence.\n");
+	printf("This is a test %z.\n");
 	return (0);
 }
