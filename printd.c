25bd51fc26291c8ec012aa79548feb98bc1e4a32
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-29 21:38:58 +0000

    Features: fixed output for unsigned range

--- printd.c
+++ printd.c
@@ -6,12 +6,15 @@
  */
 void _printd(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n1 = -n;
 	}
-	if (n / 10)
-		_printd(n / 10);
-	_putchar(n % 10 + '0');
+	n1 = n;
+	if (n1 / 10)
+		_printd(n1 / 10);
+	_putchar(n1 % 10 + '0');
 }
--- test/main.c
+++ test/main.c
@@ -13,8 +13,8 @@ int main(void)
 	/**_printf("This is a test: %z - %% - % - % c - \\ - \" - \' - %\n", 'A');
 	printf("This is a test: %z - %% - % - % c - \\ - \" - \' - %\n", 'A');**/
 
-	int n = _printf("%d\n", 0000);
-	int m = printf("%d\n", 0000);
+	int n = _printf("%d\n", INT_MIN);
+	int m = printf("%d\n", INT_MIN);
 
 	printf("%d, %d\n", n, m);
 
