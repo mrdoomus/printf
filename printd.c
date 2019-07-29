1a57c07e69e88594a7e53535da06a6fa2308a906
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-29 21:15:11 +0000

    Reset commit 321fbd4 for branch Features

--- printd.c
+++ printd.c
@@ -4,8 +4,13 @@
  * Return: void
  * @n: number
  */
-void _printd(unsigned int n)
+void _printd(int n)
 {
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 	if (n / 10)
 		_printd(n / 10);
 	_putchar(n % 10 + '0');
