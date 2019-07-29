c58b4e77791f3aeb267327e6c3c20555666a61bc
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-29 22:05:32 +0000

    master: Deleted merge warning data

--- test/main.c
+++ test/main.c
@@ -13,13 +13,8 @@ int main(void)
 	/**_printf("This is a test: %z - %% - % - % c - \\ - \" - \' - %\n", 'A');
 	printf("This is a test: %z - %% - % - % c - \\ - \" - \' - %\n", 'A');**/
 
-<<<<<<< HEAD
-	int n = _printf("%d\n", 0000);
-	int m = printf("%d\n", 0000);
-=======
 	int n = _printf("%d\n", INT_MIN);
 	int m = printf("%d\n", INT_MIN);
->>>>>>> Features
 
 	printf("%d, %d\n", n, m);
 
