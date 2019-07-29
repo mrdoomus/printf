a1b61f9de77fc3506bbf22ccc5c1d7d022ea35d3
joshuaciencia <joshuaciencia2016@gmail.com>
2019-07-29 21:56:07 +0000

    added untracked file printd.c

--- printd.c
+++ printd.c
@@ -6,16 +6,6 @@
  */
 void _printd(int n)
 {
-<<<<<<< HEAD
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		_printd(n / 10);
-	_putchar(n % 10 + '0');
-=======
 	unsigned int n1;
 
 	if (n < 0)
@@ -27,5 +17,4 @@ void _printd(int n)
 	if (n1 / 10)
 		_printd(n1 / 10);
 	_putchar(n1 % 10 + '0');
->>>>>>> Features
 }
